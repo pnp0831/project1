89065c52384da2296e1492ea25d0940883223aa1
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-30 22:14:35 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,11 +7,12 @@ import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
     CredentialsProvider({
+      id: process.env.NEXTAUTH_SECRET,
       // The name to display on the sign in form (e.g. "Sign in with...")
       name: 'Credentials',
       // `credentials` is used to generate a form on the sign in page.
@@ -24,17 +25,14 @@ export default NextAuth({
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: '1', name: credentials?.username, password: credentials?.password };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
+        console.log('credentials', credentials);
+        const user = {
+          ...credentials,
+          name: credentials.username,
+        };
+        // Validate the credentials here
+        // and return the user object if they are valid
+        return user;
       },
     }),
   ],
