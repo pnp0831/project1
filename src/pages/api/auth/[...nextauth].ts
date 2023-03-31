8226d0434baba3a3b9499e904d6a4648dd788748
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-31 18:43:13 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -69,5 +69,6 @@ const nextAuthOptions = (req, res) => {
 };
 
 export default (req, res) => {
+  console.log('defaultHeaders', defaultHeaders);
   return NextAuth(req, res, nextAuthOptions(req, res));
 };
