e4da99e28a6b59cf5e05e15d64e53270263754d6
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-31 18:18:27 +0700

    .

---
+++ src/pages/api/auth/trigger.ts
@@ -0,0 +1,9 @@
+import { setToken } from '~/helpers/axios';
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    setToken(req.body.token);
+  }
+
+  res.status(200).json({ status: 'ok' });
+}
