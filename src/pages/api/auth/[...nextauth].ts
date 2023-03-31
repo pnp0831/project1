e4f66eb23be05c272468ac360c719bcddfc27fa1
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-31 11:36:41 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,30 +7,34 @@ import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   providers: [
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: 'Credentials',
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        console.log('credentials', credentials);
-        const user = {
-          ...credentials,
-          email: credentials.username,
-          name: credentials.username,
-        };
-        // Validate the credentials here
-        // and return the user object if they are valid
-        return user;
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    // CredentialsProvider({
+    //   id: process.env.NEXTAUTH_SECRET,
+    //   // The name to display on the sign in form (e.g. "Sign in with...")
+    //   name: 'Credentials',
+    //   // `credentials` is used to generate a form on the sign in page.
+    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+    //     password: { label: 'Password', type: 'password' },
+    //   },
+    //   async authorize(credentials, req) {
+    //     // Add logic here to look up the user from the credentials supplied
+    //     console.log('credentials', credentials);
+    //     const user = {
+    //       ...credentials,
+    //       name: credentials.username,
+    //     };
+    //     // Validate the credentials here
+    //     // and return the user object if they are valid
+    //     return user;
+    //   },
+    // }),
   ],
   cookies: {
     domain: '.vercel.app',
@@ -45,7 +49,7 @@ export default NextAuth({
     async redirect({ url, baseUrl }) {
       console.log('url', url);
       console.log('baseUrl', baseUrl);
-      return baseUrl;
+      return url;
     },
   },
 });
