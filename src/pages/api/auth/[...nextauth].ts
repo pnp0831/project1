41a0f7cbd607ddef6d916e5c91eaf0b8175c3c75
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-01 22:37:20 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,7 +20,7 @@ const nextAuthOptions = (req: NextApiRequest, res: NextApiResponse) => {
           httpOnly: false,
           sameSite: 'lax',
           path: '/',
-          domain: 'pam-project1.vercel.app',
+          domain: '.vercel.app',
           secure: useSecureCookies,
         },
       },
