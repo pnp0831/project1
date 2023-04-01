5bd12bd0439d1ac112d0145598a32a64e35067b2
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-01 12:03:59 +0700

    .

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     domain:
       process.env.NODE_ENV === 'development'
         ? 'localhost'
-        : process.env.NEXT_DOMAIN || '.vercel.app',
+        : process.env.NEXT_PUBLIC_AUTH_URL || '.vercel.app',
     httpOnly: false,
   },
 };
