4394973dda61925e4b718beaa15f394998271f46
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-01 14:07:29 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
 import request, { setToken, defaultHeaders } from '~/helpers/axios';
 import { serialize, parse } from 'cookie';
 import config from '~/constants/config';
@@ -9,84 +10,99 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const nextAuthOptions = (req: NextApiRequest, res: NextApiResponse) => {
   return {
     providers: [
-      CredentialsProvider({
-        id: process.env.NEXTAUTH_SECRET,
-        name: 'Credentials',
-        credentials: {
-          username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-          password: { label: 'Password', type: 'password' },
-        },
-        async authorize(credentials, req) {
-          const { user, accessToken } = await request.post(
-            `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/signin`,
-            {
-              username: credentials?.username,
-              password: credentials?.password,
-            }
-          );
-
-          const serializedCookie = serialize('accessToken', accessToken, config.cookieConfig);
-          res.setHeader('Set-Cookie', serializedCookie);
-
-          try {
-            setToken(accessToken);
-          } catch (e) {
-            console.log('e', e);
-          }
-
-          return user;
-        },
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
+      // CredentialsProvider({
+      //   id: process.env.NEXTAUTH_SECRET,
+      //   name: "Credentials",
+      //   credentials: {
+      //     username: { label: "Username", type: "text", placeholder: "jsmith" },
+      //     password: { label: "Password", type: "password" },
+      //   },
+      //   async authorize(credentials, req) {
+      //     const { user, accessToken } = await request.post(
+      //       `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/signin`,
+      //       {
+      //         username: credentials?.username,
+      //         password: credentials?.password,
+      //       }
+      //     );
+
+      //     const serializedCookie = serialize(
+      //       "accessToken",
+      //       accessToken,
+      //       config.cookieConfig
+      //     );
+      //     res.setHeader("Set-Cookie", serializedCookie);
+
+      //     try {
+      //       setToken(accessToken);
+      //     } catch (e) {
+      //       console.log("e", e);
+      //     }
+
+      //     return user;
+      //   },
+      // }),
     ],
-    events: {
-      async signOut({ session, token }) {
-        await request.post(
-          `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/signout`,
-          {},
-          {
-            headers: defaultHeaders,
-          }
-        );
-        setToken();
-        const serializedCookie = serialize('accessToken', null, {
-          ...config.cookieConfig,
-          maxAge: 0,
-        });
+    // events: {
+    //   async signOut({ session, token }) {
+    //     await request.post(
+    //       `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/signout`,
+    //       {},
+    //       {
+    //         headers: defaultHeaders,
+    //       }
+    //     );
+    //     setToken();
+    //     const serializedCookie = serialize("accessToken", null, {
+    //       ...config.cookieConfig,
+    //       maxAge: 0,
+    //     });
 
-        res.setHeader('Set-Cookie', serializedCookie);
-      },
-    },
-    callbacks: {
-      async signIn({ user, account, profile }) {
-        return true;
-      },
-      async session({ session, token }) {
-        const cookieHeader = req.headers.cookie;
+    //     res.setHeader("Set-Cookie", serializedCookie);
+    //   },
+    // },
+    // callbacks: {
+    //   async signIn({ user, account, profile }) {
+    //     return true;
+    //   },
+    //   async session({ session, token }) {
+    //     const cookieHeader = req.headers.cookie;
 
-        // Parse the cookie header into an object
-        const cookies = cookieHeader ? parse(cookieHeader) : {};
+    //     // Parse the cookie header into an object
+    //     const cookies = cookieHeader ? parse(cookieHeader) : {};
 
-        const { user } = await request.get(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/session`, {
-          headers: {
-            accessToken: cookies.accessToken,
-          },
-        });
+    //     const { user } = await request.get(
+    //       `${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/session`,
+    //       {
+    //         headers: {
+    //           accessToken: cookies.accessToken,
+    //         },
+    //       }
+    //     );
 
-        if (cookies.accessToken) {
-          const serializedCookie = serialize('accessToken', accessToken, config.cookieConfig);
-          res.setHeader('Set-Cookie', serializedCookie);
-        }
+    //     if (cookies.accessToken) {
+    //       const serializedCookie = serialize(
+    //         "accessToken",
+    //         accessToken,
+    //         config.cookieConfig
+    //       );
+    //       res.setHeader("Set-Cookie", serializedCookie);
+    //     }
 
-        if (user?.id) {
-          return { user };
-        }
+    //     if (user?.id) {
+    //       return { user };
+    //     }
 
-        return null;
-      },
-      async redirect({ url, baseUrl }) {
-        return url;
-      },
-    },
+    //     return null;
+    //   },
+    //   async redirect({ url, baseUrl }) {
+    //     return url;
+    //   },
+    // },
   };
 };
 
