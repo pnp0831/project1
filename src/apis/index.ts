d5e4e3d7e900e4c5f898acf7b26081df76fa69c5
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-04 16:20:07 +0700

    .

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import config from '~/constants/config';
 export const API_GET_CATEGORY = '/categories';
 
-export const API_GET_PRODUCT_LIST = (category: string, page: string | number) => {
+export const API_GET_PRODUCT_LIST = (category: string, page?: string | number) => {
   const url = new URL(`https://641031d1864814e5b649fc8e.mockapi.io/api/products`);
 
   if (page) {
