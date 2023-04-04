b601ad86d5df8a34aadb75e2fafbd745fdfb195a
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-04 14:37:56 +0700

    .

--- src/pages/ecommerce/[category]/index.tsx
+++ src/pages/ecommerce/[category]/index.tsx
@@ -37,72 +37,64 @@ const ProductList = ({ products = [], total }) => {
   );
 };
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { category, page = 1 } = context.query;
+// export async function getServerSideProps(context: GetServerSidePropsContext) {
+//   const { category, page = 1 } = context.query;
 
-  const categoryId = CATEGORIES.filter((i) => i.slug === category)?.[0];
+//   const categoryId = CATEGORIES.filter((i) => i.slug === category)?.[0];
 
-  if (!categoryId) {
-    return {
-      notFound: true,
-    };
-  }
-
-  const products = await request.get(API_GET_PRODUCT_LIST(page, category));
-
-  // const products = PRODUCTS.filter((item) => item.category === category);
-
-  const total = products.length;
-  const productsRender = products.slice((page - 1) * LIMIT, page * LIMIT);
-
-  return {
-    props: {
-      products: productsRender,
-      total,
-    },
-  };
-}
-
-// export async function getStaticPaths() {
-//   // const response = await request.get(API_GET_CATEGORY);
-
-//   // const paths = response.map((post) => {
-//   //   return {
-//   //     params: { category: `${post.name}` },
-//   //   };
-//   // });
-
-//   const paths = CATEGORIES.map((item) => {
+//   if (!categoryId) {
 //     return {
-//       params: {
-//         category: item.slug,
-//       },
+//       notFound: true,
 //     };
-//   });
+//   }
 
-//   return {
-//     paths: paths,
-//     fallback: true,
-//   };
-// }
+//   const products = await request.get(API_GET_PRODUCT_LIST(page, category));
 
-// export async function getStaticProps(context) {
-//   const { params } = context;
-
-//   const products = await request.get(API_GET_PRODUCT_LIST(params.category));
-
-//   // const products = PRODUCTS.filter((item) => item.category === params.category);
+//   // const products = PRODUCTS.filter((item) => item.category === category);
 
 //   const total = products.length;
-
-//   // const productsRender = products.slice((page - 1) * LIMIT, page * LIMIT);
+//   const productsRender = products.slice((page - 1) * LIMIT, page * LIMIT);
 
 //   return {
 //     props: {
-//       products,
+//       products: productsRender,
 //       total,
 //     },
 //   };
 // }
+
+export async function getStaticPaths() {
+  const paths = CATEGORIES.map((item) => {
+    return {
+      params: {
+        category: item.slug,
+      },
+    };
+  });
+
+  return {
+    paths: paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const products = await request.get(API_GET_PRODUCT_LIST(params.category));
+
+  // const products = PRODUCTS.filter((item) => item.category === params.category);
+
+  const total = products.length;
+
+  // const productsRender = products.slice((page - 1) * LIMIT, page * LIMIT);
+
+  return {
+    props: {
+      products,
+      total,
+    },
+  };
+}
 
 export default ProductList;
