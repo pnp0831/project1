88562dc7d1ffc8ddc8cca7b26637df43ce3c8171
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-04 16:28:42 +0700

    .

--- src/pages/ecommerce/[category]/index.tsx
+++ src/pages/ecommerce/[category]/index.tsx
@@ -91,7 +91,7 @@ export async function getStaticProps(context) {
 
   const { category, page = 1 } = params;
 
-  const products = await request.get(API_GET_PRODUCT_LIST(params.category, page));
+  const products = await request.get(API_GET_PRODUCT_LIST(params.category));
 
   // const products = PRODUCTS.filter((item) => item.category === params.category);
 
